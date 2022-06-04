b0b3bebd3371ee1b0f6d7c61043d16640a7fda26
Subrat <logosubrat@gmail.com>
2022-06-04 16:21:09 -0400

    EmployeeIn4 still not complete

--- src/project/EmployeeIn4.java
+++ src/project/EmployeeIn4.java
@@ -9,34 +9,37 @@ public class EmployeeIn4 {
 
 
         int choice;
-        String answer = null;
+        String answer;
         Scanner scan = new Scanner(System.in);
 
-        do {
+        System.out.println(" . . . . . . . . . Welcome to Software Company . . . . . . . . . . .");
+        System.out.println("Please Enter the name of the Employee: ");
+        String empName = scan.nextLine();
+        System.out.println("Please Enter the Employee Department: ");
+        String empDprmt = scan.nextLine();
+        System.out.println("Please Enter the ID of Employee: ");
+        int empId = scan.nextInt();
 
-
-            System.out.println(" . . . . . . . . . Welcome to Software Company . . . . . . . . . . .");
-
-            System.out.println("Please Enter the name of the Employee: ");
-            String empName = scan.nextLine();
-            System.out.println("Please Enter the Employee Department: ");
-            String empDprmt = scan.nextLine();
-            System.out.println("Please Enter the ID of Employee: ");
-            int empId = scan.nextInt();
-
-
-            System.out.println("Employee Name: " + empName);
-            System.out.println("Employee ID: " + empId);
-            System.out.println("Employee Department: " + empDprmt);
-
-            System.out.println("do you want to convert other amount? 1- Yes, 2- No");
-           //answer = sc.nextInt();
-
-
-        }while (answer == 1);
-
-        System.out.println("Subrat keep on trying");
+        System.out.println("Employee Name: " + empName);
+        System.out.println("Employee ID: " + empId);
+        System.out.println("Employee Department: " + empDprmt);
 
     }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
