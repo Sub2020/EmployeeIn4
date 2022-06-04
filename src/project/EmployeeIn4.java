770bdb22f87cd555b6461ecf5584f066ba7d4157
Subrat <logosubrat@gmail.com>
2022-06-04 02:23:22 -0400

    EmployeeIn4

--- src/project/EmployeeIn4.java
+++ src/project/EmployeeIn4.java
@@ -5,37 +5,36 @@ import java.util.Scanner;
 public class EmployeeIn4 {
     public static void main(String[] args) {
 
-        int mode;
+
+        int choice;
+        String answer = null;
         Scanner scan = new Scanner(System.in);
 
-
-        System.out.println(" . . . . . . . . . Welcome to Software Company . . . . . . . . . . .");
-
-
-        System.out.println("Please Enter the name of the Employee: ");
-        String empName = scan.nextLine();
-        System.out.println("Please Enter the Employee Department: ");
-        String empDprmt = scan.nextLine();
-        System.out.println("Please Enter the ID of Employee: ");
-        int empId = scan.nextInt();
+        do {
 
 
-        System.out.println("Employee Name: "  + empName);
-        System.out.println("Employee ID: " + empId);
-        System.out.println("Employee Department: " + empDprmt);
-        System.out.println();
-        //System.out.println("Please Enter 1 for More Employee information");
+            System.out.println(" . . . . . . . . . Welcome to Software Company . . . . . . . . . . .");
 
-        {
-            System.out.println("Please Enter 1 for More Employee information");
-            System.out.println("Please Enter 2 to Exit");
-
+            System.out.println("Please Enter the name of the Employee: ");
+            String empName = scan.nextLine();
+            System.out.println("Please Enter the Employee Department: ");
+            String empDprmt = scan.nextLine();
+            System.out.println("Please Enter the ID of Employee: ");
+            int empId = scan.nextInt();
 
 
+            System.out.println("Employee Name: " + empName);
+            System.out.println("Employee ID: " + empId);
+            System.out.println("Employee Department: " + empDprmt);
+
+            System.out.println("do you want to convert other amount? 1- Yes, 2- No");
+           //answer = sc.nextInt();
 
 
+        }while (answer == 1);
 
+        System.out.println("Subrat keep on trying");
 
-        }
     }
 }
+
